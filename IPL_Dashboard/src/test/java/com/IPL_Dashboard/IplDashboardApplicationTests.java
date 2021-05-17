f82d3b45505cd549fd6604cf0e767a309cda583c
harish <harishmuppalla33@gmail.com>
2021-05-17 09:53:37 -0400

    Spring Batch process setup

---
+++ IPL_Dashboard/src/test/java/com/IPL_Dashboard/IplDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IPL_Dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
