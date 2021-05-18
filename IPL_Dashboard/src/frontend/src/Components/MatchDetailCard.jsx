c403db44801562f4b8998995896bff52b0e3ec0b
harish <harishmuppalla33@gmail.com>
2021-05-18 13:54:15 -0400

    react routes and links

--- IPL_Dashboard/src/frontend/src/Components/MatchDetailCard.jsx
+++ IPL_Dashboard/src/frontend/src/Components/MatchDetailCard.jsx
@@ -1,12 +1,24 @@
 import {React} from 'react';
+import { Link } from 'react-router-dom';
 
 
-export const MatchDetailCard = ({match})  => {
+export const MatchDetailCard = ({match, teamName})  => {
+  if(!match) return null;
+  const otherteam = match.team1 === teamName ? match.team2 : match.team1
+  const otherTeamRoute = `/teams/${otherteam}`;
+  var result ="wickets";
+  if(match.team1 === match.winner){
+    result ="runs";
+  }
   return (
     <div className="TeamPage">
     
      <h3>latest match</h3>
-     <h4>{match.team1} vs {match.team2}</h4>
+     <h1>vs <Link to={otherTeamRoute}> {otherteam}</Link></h1>
+     <h2>{match.date}</h2>
+     <h3>{match.venue}</h3>
+     <h3>{match.winner} won by {match.result_margin} {result}</h3>
+
     </div>
   );
 }
--- IPL_Dashboard/src/frontend/src/Components/MatchSmallCard.jsx
+++ IPL_Dashboard/src/frontend/src/Components/MatchSmallCard.jsx
@@ -1,10 +1,21 @@
 import {React} from 'react';
+import { Link } from 'react-router-dom';
 
-
-export const MatchSmallCard = ({match})  => {
+export const MatchSmallCard = ({match, teamName})  => {
+  if(!match) return null;
+  const otherteam = match.team1 === teamName ? match.team2 : match.team1
+  const otherTeamRoute = `/teams/${otherteam}`;
+  var result ="wickets";
+  if(match.team1 === match.winner){
+    result ="runs";
+  }
+    
+ 
+  
   return (
     <div className="TeamPage">
-     <p>{match.team1} vs {match.team2}</p>
+     <h3>vs <Link to={otherTeamRoute}> {otherteam}</Link></h3>
+     <p>{match.winner} won by {match.result_margin} {result}</p>
      
     </div>
   );
